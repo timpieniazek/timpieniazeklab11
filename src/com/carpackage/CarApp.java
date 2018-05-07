fc79b4ddcfc86c5d71e8d9fbbcfd3d32ec23aacd
Tim Pieniazek <timpieniazek@gmail.com>
2018-05-07 14:45:04 -0400

    Set up input while loop

--- src/com/carpackage/CarApp.java
+++ src/com/carpackage/CarApp.java
@@ -1,10 +1,24 @@
 package com.carpackage;
 
+import java.util.Scanner;
+
 public class CarApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("Welcome to the Grand Circus Motors admin console!");
+		
+		String cont = "y";
+		while (cont.equalsIgnoreCase("y")) {
+			
+			
+			cont = Validator.getYesOrNo(scan, "Would you like to continue (y/n)? ");
+		}
+		
+		System.out.println("\nThank you!\nGoodbye! :)");
+		
+		scan.close();
 	}
 
 }
